0513c2bd30b33c6274337e717fc8c6d0b11139a5
Rahma <rahmareyaz91@gmail.com>
2024-04-08 23:57:50 +0400

    chapter 5

--- Chapter-5-Embedded Content/Exercises/Bonus/index.js
+++ Chapter-5-Embedded Content/Exercises/Bonus/index.js
@@ -1,15 +1,26 @@
 document.addEventListener('DOMContentLoaded', function(){
+
+    //Select all elements with the class sample
     const samples= document.querySelectorAll('.Sample');
 
+    //add click event listener to each sample
     samples.forEach(Sample => {
         Sample.addEventListener('click', function(){
+
+            //retrieve the audio-path attribute value
             const audioPath=this.getAttribute('audio-path');
+
+            //call the playAudio function with the audio path
             playAudio(audioPath);
     });
 });
 
+//function to play the audio
 function playAudio(audioPath){
+    //create a new audio object with the provided audio path
     const audio = new Audio(audioPath);
+
+    //play the audio
     audio.play();
     }
 });
